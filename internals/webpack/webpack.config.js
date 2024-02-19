2dd7c2d86836d2ec2712276f3f0f4735f93e48f2
legobt <6wbvkn0j@anonaddy.me>
2024-02-19 05:26:00 +0000

    devDeps: bump uglifyjs-webpack-plugin

--- internals/webpack/webpack.config.js
+++ internals/webpack/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 var env = process.env.NODE_ENV;   // eslint-disable-line
 var filename = 'ethjs-util';      // eslint-disable-line
@@ -38,19 +39,15 @@ var config = {                    // eslint-disable-line
 if (env === 'production') {
   config.output.filename = filename + '.min.js'; // eslint-disable-line
   config.plugins
-    .push(new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        warnings: false,
-        screw_ie8: false,
-      },
-      mangle: {
-        screw_ie8: false,
-      },
-      output: {
-        screw_ie8: false,
+    .push(new UglifyJsPlugin({
+      uglifyOptions: {
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          warnings: false,
+        },
+        screw_ie8: true,
       },
     }));
 }
